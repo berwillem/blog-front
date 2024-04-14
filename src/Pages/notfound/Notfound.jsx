f04fa5afd5b787884040dab28f4210114a6a8349
ImeneDehimi <imenedehimi8@gmail.com>
2024-04-14 12:18:19 +0100

    404-notfound

---
+++ src/Pages/notfound/Notfound.jsx
@@ -0,0 +1,11 @@
+import './Notfound.css';
+
+const Notfound = () => {
+    return (
+        <div className='not-found'>
+            
+        </div>
+    );
+};
+
+export default Notfound;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Register from "./Pages/Register/Register.jsx";
 import Login from "./Pages/Login/Login.jsx";
+import Notfound from "./Pages/notfound/Notfound.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/Register",
     element: <Register />,
+  },
+  {
+    path: "*",
+    element: <Notfound />,
   },
 ]);
 
