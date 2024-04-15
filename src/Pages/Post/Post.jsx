fc3fec2920b975fd7d335af1a7f59aa626343d46
berchiche willem <berwillempro@gmail.com>
2024-04-15 15:13:37 +0100

    fixes

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,32 +1,40 @@
-import { useState } from "react";
-import "./Post.css"
+import React, { useState } from "react";
+import "./Post.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const Post = () => {
   const [newComment, setNewComment] = useState("");
   const [comments, setComments] = useState([]);
-  const handdleSubmit = (e) => {
+  const [editorHtml, setEditorHtml] = useState("");
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (newComment.trim() !== "") {
       setComments([...comments, newComment]);
       setNewComment("");
     }
   };
+
   const handleChange = (e) => {
     setNewComment(e.target.value);
   };
+
+  const handleEditorChange = (html) => {
+    setEditorHtml(html);
+  };
+
   return (
     <>
-      <form className="postForm" onSubmit={handdleSubmit} >
-        <label>
-          Posts
-          
-        </label>
+      <form className="postForm" onSubmit={handleSubmit}>
+        <label>Posts</label>
         <textarea
-            name="posts"
-            cols="30"
-            rows="10"
-            value={newComment}
-            onChange={handleChange}
-          />
+          name="posts"
+          cols="30"
+          rows="10"
+          value={newComment}
+          onChange={handleChange}
+        />
         <button type="submit">Add New Post</button>
       </form>
       <ul className="postList">
@@ -34,6 +42,11 @@ const Post = () => {
           <li key={index}>{comment}</li>
         ))}
       </ul>
+      <ReactQuill
+        theme="snow"
+        value={editorHtml}
+        onChange={handleEditorChange}
+      />
     </>
   );
 };
